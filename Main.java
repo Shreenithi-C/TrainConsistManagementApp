81e273205957c3be085cde499c3b60ef6615e932
Shreenithi-C <shreenithii3003@gmail.com>
2026-03-10 14:22:16 +0530

    Coded UC1 to initialize train and display consist summary

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.TrainConsistManagementApp.consist;
+
+/*
+ * @author developer
+ * @version 1.0
+ */
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		System.out.println("====================================");
+		System.out.println("=== Train Consist Management App ===");
+		System.out.println("====================================");
+		
+		List<String> trainConsist = new ArrayList<>();
+		System.out.println("\nTrain initialized successfully...");
+		System.out.println("Initial Bogie Count: "+trainConsist.size());
+		System.out.println("Current Train Consist: "+trainConsist);
+		System.out.println("\nSystem ready for operations...");
+		
+	}
+}
